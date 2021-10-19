164c0d4d8e86f3e6187f58594a953a959d994dce
brygoh <72204360+brygoh@users.noreply.github.com>
2021-10-19 09:40:51 +0800

    Version 1
    - Schema done
    - GET / POST routes initialized
    - Server hosted

---
+++ models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  name: {
+      type: String,
+      required: true,
+      unique: false,
+      trim: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    unique: false,
+    trim: true,
+  }
+}, {
+  timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ routes/users.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+let User = require('../models/user.model.js');
+const mongoose = require('mongoose');
+
+router.route('/').get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const email = req.body.email;
+  const name = req.body.name;
+  const role = req.body.role;
+
+  const newUser = new User({email, name, role});
+
+  newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+  .then(() => res.json('User deleted.'))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      user.email = req.body.email;
+      user.name = req.body.name;
+      user.role = req.body.role;
+
+      user.save()
+        .then(() => res.json('User updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
